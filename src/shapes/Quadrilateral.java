dc1a8e6b794f7010f9b86c8bc3ad7ec377bc9e2c
John Castro <johhncaastro@gmail.com>
2022-04-06 16:01:38 -0500

    finish interfaces and abstract classes exercises

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -4,6 +4,8 @@ public abstract class Quadrilateral extends Shape implements Measurable {
         protected double length;
         protected double width;
 
+        public Quadrilateral(){}
+
         public Quadrilateral(double length, double width) {
                this.length = length;
                this.width= width;
--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -1,4 +1,4 @@
 package shapes;
-
+// this class does nothing this is just simply an abstract class
 abstract class Shape {
 }
