6d6419ada12752d1273a6b24c1e4fd54eb92a137
John Castro <johhncaastro@gmail.com>
2022-04-08 12:03:24 -0500

    add practice directory

---
+++ src/PracticeJava/Practice.java
@@ -0,0 +1,13 @@
+package PracticeJava;
+
+import java.util.Arrays;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println(subtractTen(20));
+        System.out.println(subtractTen(30));
+    }
+    public static Integer subtractTen(Integer num){
+        return num -10;
+    }
+}
--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class GradesApplication {
     public static void main(String[] args) {
         HashMap<String,Student> students = new HashMap<>();
-        Student s1 = new Student("john");
+        Student s1 = new Student("johndagoat");
         s1.addGrade(100);
         s1.addGrade(100);
         s1.addGrade(100);
@@ -16,7 +16,7 @@ public class GradesApplication {
 //        System.out.println("s1.getGradeAverage() = " + s1.getGradeAverage());
         
         
-        Student s2 = new Student("goog");
+        Student s2 = new Student("googlethedog");
         s2.addGrade(23);
         s2.addGrade(49);
         s2.addGrade(89);
@@ -26,7 +26,7 @@ public class GradesApplication {
 //        System.out.println("s2.getGradeAverage() = " + s2.getGradeAverage());
 
 
-        Student s3 = new Student("bug");
+        Student s3 = new Student("bugMAN");
         s3.addGrade(50);
         s3.addGrade(78);
         s3.addGrade(13);
@@ -36,7 +36,7 @@ public class GradesApplication {
 //        System.out.println("s3.getGradeAverage() = " + s3.getGradeAverage());
 
 
-        Student s4 = new Student("felix");
+        Student s4 = new Student("xqcOW");
         s4.addGrade(13);
         s4.addGrade(1);
         s4.addGrade(23);
@@ -44,25 +44,25 @@ public class GradesApplication {
 //        System.out.println("s4.getName() = " + s4.getName());
 //        System.out.println("s4.getGrades() = " + s4.getGrades());
 //        System.out.println("s4.getGradeAverage() = " + s4.getGradeAverage());
-        System.out.println(students);
+//        System.out.println(students);
         System.out.println("Welcome!\n" +
                 "\n" +
                 "Here are the GitHub usernames of our students:\n" +
                 "\n" +
-                "|john| |goog| |bug| |felix|");
+                "|johndagoat| |googlethedog| |bugMAN| |xqcOW|");
         System.out.println("\nWhat student would you like to see more information on?\n If no say 'bye'");
         Scanner scanner = new Scanner(System.in);
 
         while(true){
             String userResponse = scanner.nextLine().trim();
-            if (userResponse.toLowerCase().equals("john")){
+            if (userResponse.toLowerCase().equals("johndagoat")){
                 System.out.println("Name: John - GitHub UserName:"+ s1.getName()+ "\n Grade Avg: "+ s1.getGradeAverage());
-            } else if (userResponse.toLowerCase().equals("goog")){
+            } else if (userResponse.toLowerCase().equals("googlethedog")){
                 System.out.println("Name: Google - GitHub UserName:"+ s2.getName()+ "\n Grade Avg: "+ s2.getGradeAverage());
-            } else if (userResponse.toLowerCase().equals("bug")){
-                System.out.println("Name: BugMan - GitHub UserName:"+ s3.getName()+ "\n Grade Avg: "+ s3.getGradeAverage());
-            } else if (userResponse.toLowerCase().equals("felix")){
-                System.out.println("Name: xqcOW - GitHub UserName:"+ s4.getName()+ "\n Grade Avg: "+ s4.getGradeAverage());
+            } else if (userResponse.toLowerCase().equals("bugman")){
+                System.out.println("Name: bug - GitHub UserName:"+ s3.getName()+ "\n Grade Avg: "+ s3.getGradeAverage());
+            } else if (userResponse.toLowerCase().equals("xqcow")){
+                System.out.println("Name: felix - GitHub UserName:"+ s4.getName()+ "\n Grade Avg: "+ s4.getGradeAverage());
             } else if (userResponse.toLowerCase().equals("bye")) {
                 System.out.println("see ya");
                 break;
